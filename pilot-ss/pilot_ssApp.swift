b116df7eb3dd3295d7b8af2328d8dfce7846d255
Shamil Chomaev <sham.chom11@gmail.com>
2023-01-03 21:48:13 +0300

    Initial Commit

---
+++ pilot-ss/pilot_ssApp.swift
@@ -0,0 +1,17 @@
+//
+//  pilot_ssApp.swift
+//  pilot-ss
+//
+//  Created by Shamil Chomaev on 03.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct pilot_ssApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
